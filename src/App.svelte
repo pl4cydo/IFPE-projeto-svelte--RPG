5a5318447fd8be5178604d2bd187f1de4af1ef0a
pl4cydo <placydo.doc@gmail.com>
2022-06-22 00:10:34 -0300

    passando muita raiva configurando o comando curl para configurar o comando npm para poder rodar o svelte

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+	let name = 'world';
+	import Image from 'src/imgs'
+</script>
+
+<h1>Hello {name}!</h1>
+<Image src="src/imgs/player1.png"/>
+<!-- <main class="fundo">
+	<div class="player1">
+		
+	
+	</div>
+		
+	<div class="player2">
+	</div>
+</main>
+
+<style>
+	.player1 {
+		
+	}
+</style> -->
