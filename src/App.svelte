d6cfb8f15fe122109f2280f5f7ec0c4f04add2e3
pl4cydo <placydo.doc@gmail.com>
2022-06-23 18:52:53 -0300

    modificações de grandes mudanças e um bom inicio

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,5 @@
 <script>
-	let name = 'world';
-	import Image from 'src/imgs'
+	import Batalha from './batalha.svelte'
 </script>
-
-<h1>Hello {name}!</h1>
-<Image src="src/imgs/player1.png"/>
-<!-- <main class="fundo">
-	<div class="player1">
-		
 	
-	</div>
-		
-	<div class="player2">
-	</div>
-</main>
-
-<style>
-	.player1 {
-		
-	}
-</style> -->
+	<Batalha/>
---
+++ src/batalha.svelte
@@ -0,0 +1,58 @@
+<svelte:head>
+	<link rel="stylesheet" href="styles/batalha.css">
+</svelte:head>
+
+<main id="main">
+	<div id="container">
+		<div id="bloco1">
+
+			<!-- Parte de Cima da Tela -->
+			<div id="topRow">
+
+				<!-- PLAYER1 -->
+				<img src="images/backgound.png" id="imageBackground">
+				<div id="blocoPersonagem">
+
+					<div id="hpp1">
+						<span>HP:</span><br>
+						<span>Super Marlon</span>
+						<div class="barraHP">
+							<div id="lifep1"></div>
+						</div>
+
+					</div>
+					<div id="player1">
+						<img src="images/player1.png">
+					</div>
+
+					<!-- INIMIGO -->
+					<div id="inimigo">
+						<img src="images/boss_semfundo.png">
+					</div>
+					<div id="hpbs">
+						<span>HP:</span><br>
+						<span>MESSIAH</span>
+						<div class="barraHP">
+							<div id="lifebs"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<!-- Parte de baixo da tela -->
+			<div id="botton">
+				<div id="bottonRow1"><p>Let's battle begins!</p></div>
+				<div id="bottonRow2">
+					<span class="Texto"> Movimentos</span><br>
+					<button>Murro</button>
+					<button>Power!</button>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</main>
+
+<script type="text/javascript">
+	import svelte from inicio_de_batalha
+</script>
