b1720dc612599df023b5cb2f0be07ee7a342fec3
pl4cydo <placydo.doc@gmail.com>
2022-06-25 03:11:25 -0300

    tantando usar o canvas para fazer o mapa, não deu certo

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,10 +1,11 @@
 <script>
 	// importa os componentes que representam os elementos gráficos do jogo
 	import Batalha from './Batalha.svelte'
-	// import Sobre from './Sobre.svelte'
 	import Menu from './Menu.svelte'
+	import Mapa from './Mapa.svelte'
 
 </script>
 	
-	<!-- <Menu/> -->
-	<Batalha/>
+	<Mapa/>
+	<!-- <Menu/>
+	<Batalha/> -->
---
+++ svelte-app/src/Mapa.svelte
@@ -0,0 +1,38 @@
+<script type="text/javascript">
+	
+	var canvas = document.querySelector('#canvas');
+	// var context = canvas.getContext('2d');
+
+	var xPos = 0;
+	var yPos = 0;
+
+	// canvas.react(xPos, yPos, 50, 50)
+	
+</script>
+
+<main>
+	<div class="container">
+		<img src="./images/backgound.png" class="fundo">
+		<canvas id="canvas" height="300" width="1000"></canvas>
+	</div>
+</main>
+
+<style type="text/css">
+	.container {
+		width: 100%;
+		height: 100hv;
+		display: flex;
+		justify-content: center;
+		margin-top: 320px;
+	}
+	.fundo {
+		height: 300px;
+		width: 1000px;
+		position: relative;
+	}
+	
+	#canvas {
+		outline: 1px solid #000;
+		position: absolute;
+	}
+</style>
